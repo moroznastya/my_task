de766a25d6a8472135004c52484d13b8f758c45e
moroznastya <nastya.moroz2210@gmail.com>
2021-11-05 21:46:48 +0200

    code was added to file

--- laba3.c
+++ laba3.c
@@ -4,59 +4,49 @@
 
 int main()
 {
-	int n;
-	printf("Input n:");
-	scanf ("%d", &n)
-	a=(int*)malloc(n * sizeof(int))
+    int n;
+    printf("Input n:");
+    scanf("%d", &n);
+    int* a = (int*)malloc(n * sizeof(int));
 
 
-		for (int i = 0; i < n; i++)
-		{
-			printf("Input a[%d]:", i + 1);
-			scanf("%d", &a[i]);
-	    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("Input a[%d]:", i + 1);
+        scanf("%d", &a[i]);
+    }
+    int x = 1;
 
-	x = func_arr(n, a);
-	
-	prinf("x=%d", x);
+    x = func_arr(n, a);
 
+    prinf("x=%d", x);
 
-	
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 
 }
 int func_arr(int n, int* a)
 {
-	int s = 0;
-	int k = 0;
-	for (i = 0; i < n; i++)
-	{
-		int	k = -1;
 
-		for (j = 0; j < n; j++)
-		{
-			if (a[i] + a[j]) = 0;
-			break;
+    for (int i = 0; i < n; i++)
+    {
+        int  k = -1;
 
-
-
-			if j = n - 1
-				k = i;
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i] + a[j]) = 0;
+            break;
 
 
 
+            if (j = n - 1)
+                k = i;
 
 
 
-
-
-
-
-
-		}
-		if k != -1
-			return a[k];
-	}
+        }
+        if (k != -1)
+            return a[k];
+    }
 
 }
